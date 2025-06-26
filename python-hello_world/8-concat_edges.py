35b5355bf9b343dfd6906061f3a7a64c77f97359
CeyhunMemmedzade777 <memmedzadeceyhun777@gmail.com>
2025-06-26 12:11:34 +0000

    Ceyhun

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:69] + str[69:70] + str[107:111] + str[111:112] + str[0:6]
-print(str)
+str1 = str[39:69] + str[69:70] + str[107:111] + str[111:112] + str[0:6]
+print(str1)
